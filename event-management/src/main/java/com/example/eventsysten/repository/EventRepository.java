955425c2b98c1ce7d2b424e418163eafff60006e
Lekha P Satnur <athes.mountain@gmail.com>
2025-06-06 11:25:17 +0530

    Delete event-management directory

--- event-management/src/main/java/com/example/eventsysten/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.eventsysten.repository;
-
-
-import com.example.eventsysten.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
--- event-management/src/main/java/com/example/eventsysten/repository/RegistrationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.eventsysten.repository;
-
-import com.example.eventsysten.entity.Registration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {}
