378320dff4e623bc1163904aeb05f15fb2b8df11
Lekha P Satnur <athes.mountain@gmail.com>
2025-06-06 11:25:43 +0530

    Delete demo directory

--- demo/src/main/java/com.example.eventmanagemet/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eventmanagementsystem.repository;
-
-import com.eventmanagementsystem.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
-
